bc4faf2fd375dc94ea80d7bf4386dabed8fcafed
Varunrhebbar <varunrhebbar06@gmail.com>
2021-09-16 21:19:03 +0530

    first commit

--- dsa/Arrays1/Untitled1.cpp
+++ dsa/Arrays1/Untitled1.cpp
@@ -1,31 +1,31 @@
-// C++ code to linearly search x in arr[]. If x
-// is present then return its location, otherwise
-// return -1
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int search(int arr[], int n, int x)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		if (arr[i] == x)
-			return i;
-	return -1;
-}
-
-// Driver code
-int main(void)
-{
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	// Function call
-	int result = search(arr, n, x);
-	(result == -1)
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result;
-	return 0;
-}
-
+class Fatherproperty{
+public:
+	string Propvalue = "one million dollers";
+	public:
+		void getProperty()
+		{
+			cout<<"Father property->"<<this->Propvalue<<endl;
+		}
+};
+	class son : protected Fatherproperty{
+		public:
+			void getProperty()
+			{
+			cout<<"Son Property =>"<<this->Propvalue<<endl;
+			}
+	};
+	class Grandson : public Fatherproperty{
+		public:
+			void getProperty()
+			{
+			cout<<"GrandSon Property =>"<<this->Propvalue<<endl;
+			}
+	};
+	int main(){
+	Grandson s1;
+	s1.getProperty();
+		}
+	
