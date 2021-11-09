9879a20dbf57928f74ff7bd00800c3aef441305b
Varunrhebbar <varunrhebbar06@gmail.com>
2021-11-10 01:28:43 +0530

    first commit

---
+++ dsa/math/prime_no.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void primefactors(int n)
+{
+	while(n%2==0)
+	{
+		cout<<2;
+		n=n/2;
+		
+	}
+	if (n>2)
+	for(int i=3;i<=sqrt(n);i=i+2)
+	{
+		while(n%i==0)
+		{
+			cout<<" ";
+		}
+	}
+}
+int main()
+{
+	int n=20;
+	primefactors(n);
+	return 0;
+}
+
