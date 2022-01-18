c61d59fd469d2f2a165db1b6c4e20f8702a330fb
Varunrhebbar <varunrhebbar06@gmail.com>
2022-01-18 10:08:19 +0530

    first commit

--- dsa/math/combination.cpp
+++ dsa/math/combination.cpp
@@ -1,39 +1,36 @@
-//#include<iostream>
-//using namespace std;
-//
-//void combination(int arr[],int data[],int start,int end,int r,int index);
-//void printcombination(int arr[],int n,int r)
-//{
-//	int data[r];
-//	combination(arr,data,0,n-1,0,r);
-//}
-//void combination(int arr[],int data[],int start,int end,int index,int r)
-//{
-//	if(index==r)
-//	{
-//		for(int j=0;j<r;j++)
-//		cout<<data[j]<<endl;
-//	cout<<endl;
-//	return ;
-//	}
-//	for(int i=start;i<=end && end-i+1>=r-index;i++)
-//	{
-//		data[index]=arr[i];
-//		combination(arr,data,i+1,end,r,index+1);
-//	}
-//}
-//int main()
-//{
-//		int n=5;
-//		int arr[n];
-//		cout<<"\n";
-//		for(int k=0;k<n;k++)
-//		{
-//		cout<<"Enter "<<k+1<<" element: ";
-//		cin>>arr[k];
-//		}
-//		int r;
-//	cin>>r;
-//    printcombination(arr, n, r);
-//    return 0;
-//}
+#include<iostream>
+using namespace std;
+
+void combination(int arr[], int data[],	int start, int end, int index, int r);
+void printcombination(int arr[], int n, int r)
+{
+	int data[r];
+	combination(arr, data, 0, n-1, 0, r);
+}
+void combination(int arr[], int data[],
+					int start, int end,
+					int index, int r)
+{
+	if (index == r)
+	{
+		for (int j = 0; j < r; j++)
+			cout << data[j] << " ";
+		cout << endl;
+		return;
+	}
+	for (int i = start; i <= end &&
+		end - i + 1 >= r - index; i++)
+	{
+		data[index] = arr[i];
+		combination(arr, data, i+1,	end, index+1, r);
+	}
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int r;
+    cin>>r;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printcombination(arr, n, r);
+}
+
